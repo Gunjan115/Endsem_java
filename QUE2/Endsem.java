d79d0ca5d38646e6994dcf13b77d9cb1512013c8
Gunjan Kamboj <162045399+Gunjan115@users.noreply.github.com>
2025-11-18 12:17:56 +0530

    Create Endsem.java

---
+++ QUE2/Endsem.java
@@ -0,0 +1,25 @@
+
+public class Endsem {
+    public static void main(String[] args) {
+
+        // Compile-time 
+        // System.out.println(x);  
+        // ERROR: x cannot be resolved
+
+
+        // Runtime 
+        int a = 10;
+        int b = 0;
+
+        int c = a / b;  
+
+        System.out.println(c);
+    }
+}
+
+
+
+
+
+
+
